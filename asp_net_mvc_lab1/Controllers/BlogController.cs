85bf891b619e81a8559a319ea705547b3a7b7ded
Mateusz Samek <mateusz.samek@microsoft.wsei.edu.pl>
2025-10-05 16:09:01 +0200

    feat: Init project 4/5 tasks

---
+++ asp_net_mvc_lab1/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using asp_net_mvc_lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_lab1.Controllers;
+
+public class BlogController : Controller
+{
+    private readonly ILogger<BlogController> _logger;
+
+    private static readonly List<BlogArticleViewModel> _articles = new()
+    {
+        new BlogArticleViewModel {Title = "Welcome to My Blog", Description = "Intro post about the blog."},
+        new BlogArticleViewModel {Title = "Learning ASP .NET MVC", Description = "Basics of ASP .NET MVC."},
+        new BlogArticleViewModel {Title = "Understanding Routing", Description = "How routing works."}
+    };
+
+    public BlogController(ILogger<BlogController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_articles);
+
+    }
+
+    public IActionResult Article(string id)
+    {
+        if (string.IsNullOrWhiteSpace(id)) return NotFound();
+        var post = _articles.FirstOrDefault(p => p.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+        return View(post);
+    }
+}
---
+++ asp_net_mvc_lab1/Models/BlogArticleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace asp_net_mvc_lab1.Models;
+
+public class BlogArticleViewModel
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+}
